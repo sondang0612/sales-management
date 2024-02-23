9587099a3c0a089a57dee4b2b5c8d625507ff3e2
sonDang612 <sonhoangdang0612@gmail.com>
2024-02-23 09:40:31 +0700

    feat: auth client token

--- src/utils/authClient.jsx
+++ src/utils/authClient.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import Navigate from "../components/Navigate";
 import { useRouter } from "next/navigation";
+import useProfile from "../react-query/useProfile";
+import toast from "react-hot-toast";
 const authClient = (Component) => {
   return function ClientOnly({ children, ...rest }) {
     const router = useRouter();
     const [load, setLoad] = React.useState(false);
+    const { data: profile } = useProfile();
 
     React.useEffect(() => {
       const token = localStorage.getItem("token");
       if (!token) {
         router.replace("/");
+        toast.error("Tài khoản hết hạn!!");
       } else {
         setLoad(true);
       }
-    }, [router]);
+    }, [router, profile]);
 
     return load ? (
       <Component {...rest}>{children}</Component>
