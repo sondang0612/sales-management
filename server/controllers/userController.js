92bb94c590d4ccb66650cfe35f8ff662fe0a0842
sonDang612 <sonhoangdang0612@gmail.com>
2024-02-24 21:38:18 +0700

    fix dc loi

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -13,12 +13,12 @@ const login = catchAsync(async (req, res) => {
     return res.status(400).json({ msg: "Vui lòng nhập đủ form" });
   }
 
-  // const user = await User.findOne({ phone, role: "USER" }).select("+password");
+  const user = await User.findOne({ phone, role: "USER" }).select("+password");
 
-  // if (!user || !(await user.matchPassword(password))) {
-  //   return res.status(400).json({ msg: "SĐT hoặc mật khẩu sai!!!" });
-  // }
-  // const token = signToken(user._id);
+  if (!user || !(await user.matchPassword(password))) {
+    return res.status(400).json({ msg: "SĐT hoặc mật khẩu sai!!!" });
+  }
+  const token = signToken(user._id);
 
   return res.status(200).json({ msg: "Đăng nhập thành công", token: 123 });
 });
